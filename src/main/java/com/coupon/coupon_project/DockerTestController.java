d5ead7df4ff85e06643138b0891a06a2505c87da
happine2s <happine2s@sookmyung.ac.kr>
2025-01-01 08:20:44 +0900

    [test] ci 동작 테스트

--- src/main/java/com/coupon/coupon_project/DockerTestController.java
+++ src/main/java/com/coupon/coupon_project/DockerTestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DockerTestController {
 
-    private static final String status = "activate";
+    private static final String status = "deactivate";
 
     @GetMapping("/")
     public String test() {
